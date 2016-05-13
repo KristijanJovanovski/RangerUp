94d3c3743da4eaed7f505bacefd1d110aefb58ea
KristijanJovanovski <jovanovski.kristijan.2@students.finki.ukim.mk>
2016-05-13 03:23:43 +0200

    gameLoop create

---
+++ RangerUp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RangerUp
+{
+    public partial class Form1 : Form
+    {
+        private Image backBuffer;
+        private Graphics graphics;
+        private readonly Graphics imageGraphics;
+
+        // Timer vars.
+        private readonly long interval = (long) TimeSpan.FromSeconds(1.0/60).TotalMilliseconds;
+        private long startTime;
+        private readonly HiResTimer timer = new HiResTimer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = CreateGraphics();
+        }
+
+        public void GameLoop()
+        {
+            timer.Start();
+
+            while (Created)
+            {
+                startTime = timer.ElapsedMilliseconds;
+                GameLogic();
+                RenderScene();
+                Application.DoEvents();
+                while (timer.ElapsedMilliseconds - startTime < interval) ;
+            }
+        }
+
+        private void GameLogic()
+        {
+            //
+        }
+
+        private void RenderScene()
+        {
+            //
+            Invalidate();
+        }
+    }
+}
---
+++ RangerUp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RangerUp
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (var form1 = new Form1())
+            {
+                form1.Show();
+                form1.GameLoop();
+            }
+        }
+    }
+}
