424795f50ff76de26e502302d60ddc531f28577d
KristijanJovanovski <jovanovski.kristijan.2@students.finki.ukim.mk>
2016-05-16 01:09:56 +0200

    create game forms , highscore and game logic

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangerUp
+{
+    public class Player:IComparer<Player>
+    {
+        public String name;
+        public long score;
+
+        public Player(string _name , long _score)
+        {
+            name = _name;
+            score = _score;
+        }
+        public int Compare(Player x, Player y)
+        {
+            if (x.score > y.score)
+                return 1;
+            if (x.score < y.score)
+                return -1;
+            return x.name.CompareTo(y.name);
+        }
+    }
+}
